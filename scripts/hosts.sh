4bba32743076c9c9ea0b400518df04797d4129e0
Gat Sirivong <sirivong@gmail.com>
2020-12-14 09:16:47 -0800

    Update hosts script to itemize all hosts.

--- scripts/hosts.sh
+++ scripts/hosts.sh
@@ -3,18 +3,23 @@
 HOSTS_FILE=hosts.txt
 NODE_SET=2
 
-cmd() {
-	IP=$(multipass info ${1} | grep IPv4 | awk '{print $2}')
-	echo "${IP} ${1}" >> ${HOSTS_FILE}
+cat /dev/null > ${HOSTS_FILE}
 
+cmd() {
+	EXTRA_HOST=''
+	if [[ -n "${2}" ]] ; then
+		EXTRA_HOST=" ${2}"
+	fi
+	IP=$(multipass info ${1} | grep IPv4 | awk '{print $2}')
+	echo "${IP} ${1}${EXTRA_HOST}" >> ${HOSTS_FILE}
 }
 
 haproxy_controller() {
-	cmd haproxy-controller
+	cmd haproxy-controller cluster-endpoint
 }
 
 haproxy_etcd() {
-	cmd haproxy-etcd
+	cmd haproxy-etcd etcd-endpoint
 }
 
 controller() {
